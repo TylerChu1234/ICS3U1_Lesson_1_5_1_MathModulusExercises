c083ada8f2fb9a5995e49ab1c377e8032b1cb767
TylerChu1234 <tylerchu.tyc@gmail.com>
2024-02-16 11:36:04 -0500

    Cos3rd.java

--- Cos3rd.java
+++ Cos3rd.java
@@ -18,35 +18,35 @@ class Cos3rd extends ConsoleProgram {
     double dblCosAngle;
     double dblCosAngleP2;
     
-  dblSideA = readDouble ("Enter the side length of A: ");
-  dblSideB = readDouble ("Enter the side length of B: ");
-  AngleC = readDouble ("Enter the value for angle C: ");
+  dblSideA = readDouble ("Enter the side length of A in cm: ");
+  dblSideB = readDouble ("Enter the side length of B in cm: ");
+  AngleC = readDouble ("Enter the value for angle C in degrees: ");
 
 
-  dblCosAngle = (Math.toDegreesMath.cos(AngleC)));
-
+  dblCosAngle = (AngleC/57.2958);
+  dblCosAngleP2 = (Math.cos(dblCosAngle));
 
   
 
 
 
-  //dblSideCP1 = (Math.pow(dblSideA, 2) + (Math.pow(dblSideB, 2)));
+  dblSideCP1 = (Math.pow(dblSideA, 2) + (Math.pow(dblSideB, 2)));
 
-  //dblSideCP2 = (-2*dblSideA*dblSideB);
+  dblSideCP2 = (-2*dblSideA*dblSideB);
 
 
 
-  //dblSideCP3 = (dblSideCP2*dblCosAngle);
+  dblSideCP3 = (dblSideCP2*dblCosAngleP2);
 
-  //dblSideCP4 = (dblSideCP1 + dblSideCP3);
+  dblSideCP4 = (dblSideCP1 + dblSideCP3);
 
-  //dblSideCP5 = (Math.sqrt(dblSideCP4));
+  dblSideCP5 = (Math.sqrt(dblSideCP4));
 
     
   
  
 
-  System.out.println(dblCosAngle);
+  System.out.println("The length of side C is about " + dblSideCP5 + " cm");
 
 
 
